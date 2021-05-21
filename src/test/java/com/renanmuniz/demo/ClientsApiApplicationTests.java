ec06515ca27af7707301b4d6392b293c5f5a93bc
Renan Muniz <fmrenan@outlook.com>
2021-05-20 22:12:27 -0300

    Setup springboot project

---
+++ src/test/java/com/renanmuniz/demo/ClientsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanmuniz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
