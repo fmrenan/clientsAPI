b548d5d28f2ca6d3471f4d07b1caea0c5c017611
Renan Muniz <fmrenan@outlook.com>
2021-05-22 16:36:06 -0300

    POST client

--- src/main/java/com/renanmuniz/clients/resources/ClientResource.java
+++ src/main/java/com/renanmuniz/clients/resources/ClientResource.java
@@ -1,14 +1,20 @@
 package com.renanmuniz.clients.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.renanmuniz.clients.dto.ClientDTO;
 import com.renanmuniz.clients.services.ClientService;
@@ -36,4 +42,18 @@ public class ClientResource {
 		return ResponseEntity.ok().body(clients);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ClientDTO findById(@PathVariable Long id) {
+		return service.findById(id);
+	}
+
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+		dto = service.insert(dto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
--- src/main/java/com/renanmuniz/clients/services/ClientService.java
+++ src/main/java/com/renanmuniz/clients/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.renanmuniz.clients.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,6 +24,32 @@ public class ClientService {
 
 		return list.map(cli -> new ClientDTO(cli));
 	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> object = repository.findById(id);
+		
+		Client client = object.get();
+		
+		return new ClientDTO(client);
+	}
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		
+		entity = repository.save(entity);
+
+		return new ClientDTO(entity);
+	}
+
+	
 	
 	
 }
